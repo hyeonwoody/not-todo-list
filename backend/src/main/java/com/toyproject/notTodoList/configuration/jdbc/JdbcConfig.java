b484ae6e3fa4214d43f97276d543d833182cd3db
hyeonwoody <hyeonwoody@gmail.com>
2024-05-16 20:51:04 +0900

    🛢️DB : Add JdbcConfig

--- backend/src/main/java/com/toyproject/notTodoList/configuration/jdbc/JdbcConfig.java
+++ backend/src/main/java/com/toyproject/notTodoList/configuration/jdbc/JdbcConfig.java
@@ -1,5 +1,6 @@
 package com.toyproject.notTodoList.configuration.jdbc;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,15 +11,19 @@ import javax.sql.DataSource;
 @Configuration
 public class JdbcConfig {
 
-
+    @Getter
     @Value("${my.database.driverClassName}")
     String drvierClassName;
 
+    @Getter
     @Value("${my.database.username}")
     String username;
 
+    @Getter
     @Value("${my.database.password}")
     String password;
+
+    @Getter
     @Value("${my.database.url}")
     String url;
 
