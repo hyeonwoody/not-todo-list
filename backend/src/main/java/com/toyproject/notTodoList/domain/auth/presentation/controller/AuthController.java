a83f1fe25fe15193db71f6b81bcb2877cb140992
hyeonwoody <hyeonwoody@gmail.com>
2024-05-23 22:42:58 +0900

    📝DOCS : Add tag and operation for Swagger UI

--- backend/src/main/java/com/toyproject/notTodoList/domain/auth/presentation/controller/AuthController.java
+++ backend/src/main/java/com/toyproject/notTodoList/domain/auth/presentation/controller/AuthController.java
@@ -12,6 +12,8 @@ import com.toyproject.notTodoList.domain.auth.presentation.dto.req.LoginAPIReque
 import com.toyproject.notTodoList.domain.auth.presentation.dto.req.RegisterAPIRequest;
 import com.toyproject.notTodoList.domain.auth.presentation.dto.res.DetailLoginAPIResponse;
 import com.toyproject.notTodoList.domain.auth.presentation.dto.res.RegisterAPIResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
@@ -21,24 +23,34 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Auth API")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final AuthenticationManager authenticationManager;
     private final AuthService authService;
 
+    @Operation(summary = "Check username availability", description = "Checks if the provided username is available for registration.")
     @GetMapping("/exists/{username}")
     public ResponseEntity<String> checkDuplication (@PathVariable @NotEmpty String username) {
         authService.isUsernameTaken(username);
         return ResponseEntity.ok("사용 가능한 아이디 입니다.");
     }
 
+    @Operation(summary = "Register Member", description = "Registers a new member with the provided information. Username availability is checked before registration.")
     @PostMapping("/register")
     public ResponseEntity<RegisterAPIResponse> register (@RequestBody @Valid RegisterAPIRequest request){
         RegisterResponse response = authService.register(RegisterRequest.from(request));
         return ResponseEntity.ok(RegisterAPIResponse.from(response));
     }
 
+    @Operation(
+            summary = "Login Member",
+            description = "Attempts to authenticate a member with the provided username and password. If successful, a JWT token and login details will be returned."
+    )
     @PostMapping("/login")
     public ResponseEntity<DetailLoginAPIResponse> login (@RequestBody @Valid LoginAPIRequest request){
         JwtAuthenticationContext authToken = new JwtAuthenticationContext(request.getUsername(), request.getPassword());
--- backend/src/main/java/com/toyproject/notTodoList/domain/member/domain/entity/profile/presentation/controller/dto/ProfileController.java
+++ backend/src/main/java/com/toyproject/notTodoList/domain/member/domain/entity/profile/presentation/controller/dto/ProfileController.java
@@ -2,6 +2,8 @@ package com.toyproject.notTodoList.domain.member.domain.entity.profile.presentat
 
 import com.toyproject.notTodoList.domain.member.domain.entity.profile.application.ProfileService;
 import com.toyproject.notTodoList.domain.member.domain.entity.profile.infrastructure.ProfileJdbcTemplateRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Profile API")
 @RequestMapping("/api/v1/profile")
 @RequiredArgsConstructor
 public class ProfileController {
 
     private final ProfileService profileService;
 
+    @Operation(summary = "Check nickname availability", description = "Checks if the provided nickname is available for use.")
     @GetMapping("/exists/{nickname}")
     public ResponseEntity<String> checkDuplication (@PathVariable @NotEmpty String nickname) {
         profileService.isNicknameTaken(nickname);
